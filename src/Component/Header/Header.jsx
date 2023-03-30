d6c63485d792989bcaa66522e96c3a609612c7af
abrarazmain <ajmainabrar5@gmail.com>
2023-03-31 01:50:31 +0600

    added header

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <h1 className="header-title">PHero Cafeteria</h1>
+          <img className="header-img" src='https://thumbs.dreamstime.com/b/simple-illustration-mug-coffee-logo-cafe-restaurant-192706246.jpg' alt="" />
+    </div>
+  );
+};
+
+export default Header;
